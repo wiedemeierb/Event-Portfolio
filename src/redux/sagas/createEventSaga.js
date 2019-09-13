1f5969d51b0711c4bdbff64bb6cb2331a4d896f7
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-09-13 12:03:55 -0500

    on createEvent, userpage updates with new event

--- src/redux/sagas/createEventSaga.js
+++ src/redux/sagas/createEventSaga.js
@@ -1,10 +1,14 @@
-import { takeLatest } from 'redux-saga/effects';
+import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 function* createEvent(action) {
     try {
         //  passes created event from payload to server
         yield axios.post('/api/event/createevent', action.payload); 
+
+        yield put({
+            type: 'FETCH_USEREVENTS'})
+
     }catch(error) {
         console.log('Error with create event', error);
     }
