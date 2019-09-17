85c5510d0479dcf1bf96457178d5a8ba3224531b
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-09-17 10:08:45 -0500

    edit user info saga created with proper routes

---
+++ src/redux/sagas/EditSaga.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* editUserInfo(action) {
+    console.log('editUserInfo Saga', action.payload);
+    let id = action.payload.id
+    try{
+        let response = yield axios.put(`/api/edit${id}`, action.payload)
+        console.log('editUserInfo Saga response', response.data);
+    }catch(error) {
+        console.log('error in editUserInfo Saga', error)
+    }
+};
+
+function* editSaga(){
+    yield takeLatest('EDIT_USERINFO', editUserInfo)
+}
+
+export default editSaga;
